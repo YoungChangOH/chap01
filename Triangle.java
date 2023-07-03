76c1408fb622c214f304173fcc948def53df0c42
Chang <kjkszpj13@naver.com>
2023-07-03 12:47:36 +0900

    *>#>0

--- Triangle.java
+++ Triangle.java
@@ -7,17 +7,17 @@ class Triangle {
 
 
 	public static void main(String[] args) {
-		System.out.println("     *");
-		System.out.println("    ***");
-		System.out.println("   *****");
-		System.out.println("  *******");
-		System.out.println(" *********");
-		System.out.println("***********");
-		System.out.println(" *********");
-		System.out.println("  *******");
-		System.out.println("   *****");
-		System.out.println("    ***");
-		System.out.println("     *");
+		System.out.println("     0");
+		System.out.println("    000");
+		System.out.println("   00000");
+		System.out.println("  0000000");
+		System.out.println(" 000000000");
+		System.out.println("00000000000");
+		System.out.println(" 000000000");
+		System.out.println("  0000000");
+		System.out.println("   00000");
+		System.out.println("    000");
+		System.out.println("     0");
 
 	}
 
