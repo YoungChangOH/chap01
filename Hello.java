e467dc111487c47d635b0da04d340ecddd813bb7
Chang <kjkszpj13@naver.com>
2023-07-03 12:02:23 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+class Hello {
+
+
+
+	public static void main(String[] args) {
+		System.out,println(Hello");
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+
+
+class Rectangle {
+
+	public static void main(String[] args){
+		System.out.println("*******************.");
+		System.out.println("*****");
+		System.out.println("Rectangle Of Remove.");
+		System.out.println("Rectangle Of Remove.");
+		System.out.println("Rectangle Of Remove.");
+		System.out.println("Rectangle Of Remove.");
+		System.out.println("Rectangle Of Remove.");
+		System.out.println("Rectangle Of Remove.");
+		System.out.println("Rectangle Of Remove.");
