38110918d7b6c496f621cde74cbbedab4bd1379e
Chang <kjkszpj13@naver.com>
2023-07-06 17:18:06 +0900

    진행중

--- src/Hello.java
+++ src/Hello.java
@@ -1,34 +1,33 @@
-
-public class Hello {
+class Hello {
 
 	public static void main(String[] args){
 
-		System .out.println("\033[2J");
-		System .out.println("\033[31m");
-		System .out.println("\033[44m");
+		System.out.println("\033[2J");
+		System.out.println("\033[31m");
+		System.out.println("\033[44m");
 
-		System .out.print("\033[10;20H");
-		System .out.println("Hello Java!");
+		System.out.print("\033[10;20H");
+		System.out.println("Hello Java!");
 
-		System .out.println("\033[34m");
-		System .out.println("\033[41m");
+		System.out.println("\033[40m");
+		System.out.println("\033[45m");
 
-		System .out.print("\033[5;10H");
-		System .out.println("Hello JavaScript!");
+		System.out.print("\033[15;10H");
+		System.out.println("Hello JavaScript!");
 
-		System .out.println("\033[32m");
-		System .out.println("\033[442m");
+		System.out.println("\033[30m");
+		System.out.println("\033[47m");
 
-		System .out.print("\033[20;20H");
-		System .out.println("Hello HTML!");
+		System.out.print("\033[25;20H");
+		System.out.println("Hello HTML!");
 
-		System .out.println("\033[30m");
-		System .out.println("\033[48m");
+		System.out.println("\033[39m");
+		System.out.println("\033[46m");
 
-		System .out.print("\033[15;30H");
-		System .out.println("Hello SQL!");
+		System.out.print("\033[15;30H");
+		System.out.println("Hello SQL!");
 
-		System .out.println("\033[0m");
+		System.out.println("\033[0m");
 	}
 
 }
