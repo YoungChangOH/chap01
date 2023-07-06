871801a6676348312c66c992a14abd6f7c0602f4
YoungChang <kjkszpj13@naver.com>
2023-07-06 12:01:24 +0900

    vt 100 clear

--- src/Hello.java
+++ src/Hello.java
@@ -1,40 +1,12 @@
 
+public class Hello {
 
+	public static void main(String[] args){
 
+		System .out.println("\033[2J");
 
+		System .out.println("Hello Java");
 
-
-class Hello {
-
-
-
-	public static void main(String[] args) {
-		System.out,println(Hello");
 	}
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -7,6 +7,8 @@ class Triangle {
 
 
 	public static void main(String[] args) {
+		//삼각형 출력
+
 		System.out.println("     @");
 		System.out.println("    @@@");
 		System.out.println("   @@@@@");
