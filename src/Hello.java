d15835f3dc93d4e175a2629cad28790db754fe7f
YoungChang <kjkszpj13@naver.com>
2023-07-06 13:06:34 +0900

    JAVA 수정

--- src/Hello.java
+++ src/Hello.java
@@ -7,7 +7,26 @@ public class Hello {
 		System .out.println("\033[31m");
 		System .out.println("\033[44m");
 
+		System .out.print("\033[10;20H");
 		System .out.println("Hello Java!");
+
+		System .out.println("\033[34m");
+		System .out.println("\033[41m");
+
+		System .out.print("\033[5;10H");
+		System .out.println("Hello JavaScript!");
+
+		System .out.println("\033[32m");
+		System .out.println("\033[442m");
+
+		System .out.print("\033[20;20H");
+		System .out.println("Hello HTML!");
+
+		System .out.println("\033[30m");
+		System .out.println("\033[48m");
+
+		System .out.print("\033[15;30H");
+		System .out.println("Hello SQL!");
 
 		System .out.println("\033[0m");
 	}
