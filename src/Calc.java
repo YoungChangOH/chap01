f405186c728c6c598a99f343eb76bd520bb49ebc
YoungChang <kjkszpj13@naver.com>
2023-07-12 15:28:26 +0900

    조정2

--- src/Calc.java
+++ src/Calc.java
@@ -4,7 +4,7 @@ class Calc {
 	
 	public static void main(String[] args) {
 		int sum = 0;
-		for (int i=1; i<=102; i++) {
+		for (int i=2; i<=104; i++) {
 			sum =sum+i;
 					System.out.println(i);
 		}
