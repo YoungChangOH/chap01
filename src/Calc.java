8e696a3210c48223d32aaa5b9dc1d189a80a3d01
Chang <kjkszpj13@naver.com>
2023-07-12 15:22:53 +0900

    조정

--- src/Calc.java
+++ src/Calc.java
@@ -1,5 +1,15 @@
 class Calc {
+	
+	
+	
 	public static void main(String[] args) {
+		int sum = 0;
+		for (int i=1; i<=102; i++) {
+			sum =sum+i;
+					System.out.println(i);
+		}
+	}
+	public static void main2(String[] args) {
 		int sum=0;
 
 		for (int i=1; i<=10; i++)
